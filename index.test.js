33c1c27869c31850e6bffde81b547d5058ec6766
Enes Kahrovic <ekhrovic@gmail.com>
2019-07-05 10:19:37 +0200

    11. test refactored

--- index.test.js
+++ index.test.js
@@ -2,12 +2,6 @@ const {add} = require('./index');
 
 describe('add function', () => {
 
-    it('should add two numbers', () => {
-
-        expect(add(4, 5)).toBe(9);
-
-    });
-
     it('should add arbitrary number of numbers', () => {
 
         expect(add(4, 5, 6, 7)).toBe(22);
@@ -17,12 +11,6 @@ describe('add function', () => {
     it('should ignore falsy values', () => {
 
         expect(add(4, 5, 6, 7, null, undefined, '', NaN)).toBe(22);
-
-    });
-
-    it('should also add strings that can be coerced to numbers', () => {
-
-        expect(add('1', '2', '5')).toBe(8);
 
     });
 
