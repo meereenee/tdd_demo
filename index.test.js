2c5b4e7969b1dbcb9c025fced53a8c717b545cac
Enes Kahrovic <ekhrovic@gmail.com>
2019-07-05 10:09:30 +0200

    8. failing test for string convertable to numbers

--- index.test.js
+++ index.test.js
@@ -18,8 +18,15 @@ describe('add function', () => {
 
     it('should ignore falsy values', () => {
 
-        expect(add(4, 5, 6, 7, null, undefined,'', NaN)).toBe(22);
+        expect(add(4, 5, 6, 7, null, undefined, '', NaN)).toBe(22);
 
     });
+
+    it('should also add strings that can be coerced to numbers', () => {
+
+        expect(add('1', '2', '5')).toBe(8);
+
+    });
+
 
 });
