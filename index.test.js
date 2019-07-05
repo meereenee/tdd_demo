c5d8c567bdfc020e5c716c9e92184c8565607f62
Enes Kahrovic <ekhrovic@gmail.com>
2019-07-05 10:20:38 +0200

    just aligment fix

--- index.test.js
+++ index.test.js
@@ -3,22 +3,14 @@ const {add} = require('./index');
 describe('add function', () => {
 
     it('should add arbitrary number of numbers', () => {
-
         expect(add(4, 5, 6, 7)).toBe(22);
-
     });
 
     it('should ignore falsy values', () => {
-
         expect(add(4, 5, 6, 7, null, undefined, '', NaN)).toBe(22);
-
     });
 
     it('should ignore all types that are not strings or numbers', () => {
-
         expect(add('1', 2, [5, 6], {a: 10, b: 50}, () => 22)).toBe(3);
-
     });
-
-
 });
